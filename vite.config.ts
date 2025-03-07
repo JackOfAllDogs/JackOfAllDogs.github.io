e0027f7358913d9e7eae7e9a8d2426b14f1b7aee
Jack <94.j.blythe@gmail.com>
2025-03-07 22:03:48 +0000

    Test vite.config base options

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JackOfAllDogs.github.io/',
+  // base: '/JackOfAllDogs.github.io/',
+  base: '/',
   plugins: [react({})],
 })
