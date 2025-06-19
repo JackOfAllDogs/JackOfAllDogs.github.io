f6979980389949f20309b38102cb691f6bc65614
MariaHajdic <marianneliash@gmail.com>
2025-06-19 10:18:54 +0100

    Cleaner home image--no overlays

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -4,9 +4,7 @@ import jackImage from "../../assets/images/jack.webp"
 const Home = () => {
   return (
     <section className="home">
-      <img src={jackImage} alt="Jack" className="home-image" loading="lazy" />
-      <div className="overlay"></div> {/* Dark overlay */}
-      <div className="fade-overlay"></div> {/* Fade effect at the bottom */}
+      <img src={jackImage} alt="Jack" className="home-image" />
     </section>
   )
 }
