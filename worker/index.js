58d0439c6f52150ca69bd44000c86ec36d0958a6
MariaHajdic <marianneliash@gmail.com>
2025-08-08 14:00:37 +0100

    Redirects handled at the worker level

--- worker/index.js
+++ worker/index.js
@@ -1,38 +1,29 @@
-// import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
-// addEventListener('fetch', event => {
-//   event.respondWith(handleEvent(event));
-// });
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event));
+});
 
-// async function handleEvent(event) {
-//   try {
-//     return await getAssetFromKV(event);
-//   } catch (e) {
-//     let pathname = new URL(event.request.url).pathname;
-//     return new Response(`"${pathname}" not found`, {
-//       status: 404,
-//       statusText: "not found"
-//     });
-//   }
-// }
+async function handleEvent(event) {
+  const url = new URL(event.request.url);
+  const hostname = url.hostname;
 
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    const hostname = url.hostname;
+  const redirectTo = "https://jackofalldogs.co.uk";
 
-    const redirectTo = "https://jackofalldogs.co.uk";
-
-    // Redirect if not already at the correct domain
-    if (
-      hostname === "jackofalldogs.com" ||
-      hostname === "www.jackofalldogs.com" ||
-      hostname === "www.jackofalldogs.co.uk"
-    ) {
-      return Response.redirect(`${redirectTo}${url.pathname}${url.search}`, 301);
-    }
-
-    // Otherwise, serve the site as normal (if you have static assets)
-    return fetch(request);
+  if (
+    hostname === "jackofalldogs.com" ||
+    hostname === "www.jackofalldogs.com" ||
+    hostname === "www.jackofalldogs.co.uk"
+  ) {
+    return Response.redirect(`${redirectTo}${url.pathname}${url.search}`, 301);
   }
-};
+
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    return new Response(`"${url.pathname}" not found`, {
+      status: 404,
+      statusText: "not found"
+    });
+  }
+}
